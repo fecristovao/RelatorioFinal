beaedd4dbe59a3bd5b5e60e86d2b05e34a8639cd
Fernando Cristovao <fernandocristovao@id.uff.br>
2019-09-29 03:34:32 -0300

    Criada a view medição

---
+++ src/views/Medicao.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        {{inicio}}
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            inicio: "k"
+        }
+    }
+}
+</script>
