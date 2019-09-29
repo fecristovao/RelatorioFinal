3a8fb11b93645b113dda618faf66f05c464123b4
Fernando Cristovao <fernandocristovao@id.uff.br>
2019-09-28 22:41:07 -0300

    Inicio do Projeto

---
+++ src/views/Espelho.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        
+        {{dados["k"]}}
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            dados: {}
+        }
+    },
+
+    mounted() {
+        this.dados["k"] = "oi"
+    }
+}
+</script>
