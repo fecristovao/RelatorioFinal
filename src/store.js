775d5bb7c3852b0660438044db3bd710ddf70429
Fernando Cristovao <fernandocristovao@id.uff.br>
2019-10-01 11:23:52 -0300

    Versão de Produção

--- src/store.js
+++ src/store.js
@@ -6,11 +6,11 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     urls: {
-      espelho: "http://192.168.1.93:8049/webrunstudio/espelhoapi.rule?sys=MDC&mes=:mes&contrato=:contrato",
-      medicao: "http://192.168.1.93:8049/webrunstudio/medicaoapi.rule?sys=MDC&mes=:mes&contrato=:contrato",
-      graficos: "http://192.168.1.93:8049/webrunstudio/graficosapi.rule?sys=MDC&mes=:mes&contrato=:contrato",
-      rdo: "http://192.168.1.93:8049/webrunstudio/rdoapi.rule?sys=MDC&id=",
-      rdoIMG: "https://webrun.perbras.com.br/webapp/imgapi.rule?sys=MDC&id=:img"
+      espelho: "/webapp/espelhoapi.rule?sys=MDC&mes=:mes&contrato=:contrato",
+      medicao: "/webapp/medicaoapi.rule?sys=MDC&mes=:mes&contrato=:contrato",
+      graficos: "/webapp/graficosapi.rule?sys=MDC&mes=:mes&contrato=:contrato",
+      rdo: "/webapp/rdoapi.rule?sys=MDC&id=",
+      rdoIMG: "/webapp/imgapi.rule?sys=MDC&id=:img"
     }
   },
   mutations: {
