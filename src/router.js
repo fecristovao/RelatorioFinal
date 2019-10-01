881aee31ca167f3cb5108c4916add4de056f206b
Fernando Cristovao <fernandocristovao@id.uff.br>
2019-10-01 11:34:43 -0300

    Titulo dinâmico

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 import Espelho from './views/Espelho.vue'
 import Medicao from './views/Medicao.vue'
 import Graficos from './views/Graficos.vue'
@@ -10,19 +9,6 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
     {
       path: '/espelho/:mes/:contrato',
       name: 'Espelho',
@@ -54,4 +40,5 @@ export default new Router({
       component: RDO
     }
   ]
+  
 })
